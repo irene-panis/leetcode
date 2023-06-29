69ca2797f1f5c923087913ed171b55e0da6322bb
irene <panisirene@gmail.com>
2023-06-29 23:07:09 +0000

    [LeetCode Sync] Runtime - 56 ms (25.38%), Memory - 42.4 MB (100.00%)

---
+++ solutions/2732-counter/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let counter = 0;
+    return function() {
+        let result = n + counter;
+        counter++;
+        return result;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
