1bf5ce4ba941eb8c2840840d08f094c8255ae985
irene <panisirene@gmail.com>
2023-08-01 21:03:41 +0000

    [LeetCode Sync] Runtime - 10 ms (88.71%), Memory - 54.9 MB (94.75%)

--- solutions/0217-contains-duplicate/solution.java
+++ solutions/0217-contains-duplicate/solution.java
@@ -1,16 +1,19 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
+/*
+1. use hashset for this
+2. iterate over nums
+3. if our hashset contains current num we return true
+4. otherwise, add current num to hashset
+5. after we iterate we return false by default
+*/
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> seen = new HashSet<>(); // initialize hashset = good for unique values
-        for (int num : nums) { // for number in nums
-            if(seen.contains(num)) { // if hashset contains that number
-                return true; // return true = yes, there are duplicates
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
             }
-            seen.add(num); // add that number into hashset
+            seen.add(num);
         }
-        return false; // if we iterate over entire thing and find no dupes return false
+        return false;
     }
 }
