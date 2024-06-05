f179a3931f7a310faae53252567828681b815c21
irene <panisirene@gmail.com>
2024-06-05 00:43:32 +0000

    [LeetCode Sync] Runtime - 88 ms (30.15%), Memory - 66.8 MB (6.08%)

--- solutions/0217-contains-duplicate/solution.js
+++ solutions/0217-contains-duplicate/solution.js
@@ -3,10 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  let newNums = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (newNums.includes(nums[i])) return true;
-    newNums.push(nums[i]);
-  }
-  return false;
+    let mp = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (mp.has(nums[i])) {
+            return true;
+        }
+        mp.set(nums[i], i);
+    }
+    return false;
 };
