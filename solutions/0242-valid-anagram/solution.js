da906c21cba76c128a80f8f2b2a721e524052184
irene <panisirene@gmail.com>
2024-06-06 03:06:53 +0000

    [LeetCode Sync] Runtime - 88 ms (14.59%), Memory - 53.5 MB (43.04%)

--- solutions/0242-valid-anagram/solution.js
+++ solutions/0242-valid-anagram/solution.js
@@ -4,12 +4,14 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  if (s.length != t.length) return false;
-  for (let i = 0; i < s.length; i++) {
-    // indexOf finds first occurrence of letter in t; if == -1 then its not found, returns false
-    if (t.indexOf(s[i]) == -1) return false;
-    // replace letter in t with empty space
-    t = t.replace(s[i], '');
-  }
-  return true;
+    if (s.length !== t.length) { return false; }
+
+    const sortS = s.split("").sort();
+    const sortT = t.split("").sort();
+
+    for (let i = 0; i < sortS.length; i++) {
+        if (sortS[i] !== sortT[i]) { return false ;}
+    }
+
+    return true;
 };
