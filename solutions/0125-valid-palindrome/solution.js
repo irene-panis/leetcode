e34f5ad3ff0188cc577b15c1689d158cd4da062b
irene <panisirene@gmail.com>
2024-06-12 00:28:42 +0000

    [LeetCode Sync] Runtime - 51 ms (95.70%), Memory - 51.8 MB (70.46%)

--- solutions/0125-valid-palindrome/solution.js
+++ solutions/0125-valid-palindrome/solution.js
@@ -3,12 +3,29 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  let stringReversed = '';
-  s = s.toLowerCase().replace(/\s/g, "").replace(/[^0-9a-z]/gi, "");
-  for (let i = 0; i < s.length; i++) {
-    stringReversed = stringReversed.concat(s[s.length - 1 - i]);
-  }
-  console.log(stringReversed);
-  if (stringReversed != s) return false;
-  return true;
-};
+    let l = 0;
+    let r = s.length - 1;
+    while (l < r) {
+        while (l < r && !isAlphaNum(s[l])) {
+            l++; // continue if current char != alphanum
+        }
+        while (r > l && !isAlphaNum(s[r])) {
+            r--; // continue if current char != alphanum
+        }
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+
+var isAlphaNum = function(c) {
+    const charCode = c.charCodeAt(0);
+    return (
+        (65 <= charCode && charCode <= 90) ||
+        (97 <= charCode && charCode <= 122) ||
+        (48 <= charCode && charCode <= 57)
+    );
+}
