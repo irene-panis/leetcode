9d00cf00756962969cd0562448e9b7d49c087bf0
irene <panisirene@gmail.com>
2024-06-15 23:26:23 +0000

    [LeetCode Sync] Runtime - 56 ms (86.45%), Memory - 52.1 MB (62.79%)

--- solutions/0125-valid-palindrome/solution.js
+++ solutions/0125-valid-palindrome/solution.js
@@ -5,12 +5,13 @@
 var isPalindrome = function(s) {
     let l = 0;
     let r = s.length - 1;
+
     while (l < r) {
         while (l < r && !isAlphaNum(s[l])) {
-            l++; // continue if current char != alphanum
+            l++;
         }
         while (r > l && !isAlphaNum(s[r])) {
-            r--; // continue if current char != alphanum
+            r--;
         }
         if (s[l].toLowerCase() !== s[r].toLowerCase()) {
             return false;
@@ -21,7 +22,7 @@ var isPalindrome = function(s) {
     return true;
 }
 
-var isAlphaNum = function(c) {
+var isAlphaNum = function(c) { // func for checking if character is alphanumeric Aa-Zz 0-9
     const charCode = c.charCodeAt(0);
     return (
         (65 <= charCode && charCode <= 90) ||
