1d27156e23bcf983e1db1815e262b69dad1b92c5
irene <panisirene@gmail.com>
2024-06-15 23:22:18 +0000

    [LeetCode Sync] Runtime - 67 ms (72.06%), Memory - 51.5 MB (70.50%)

--- solutions/0242-valid-anagram/solution.js
+++ solutions/0242-valid-anagram/solution.js
@@ -7,11 +7,11 @@ var isAnagram = function(s, t) {
     if (s.length !== t.length) {
         return false;
     }
-    let sCount = {}; // store counts in objects
-    let tCount = {};
+    sCount = {};
+    tCount = {};
     for (let i = 0; i < s.length; i++) {
-        sCount[s[i]] = 1 + (sCount[s[i]] || 0);
-        tCount[t[i]] = 1 + (tCount[t[i]] || 0);
+        sCount[s[i]] = (sCount[s[i]] + 1) || 0;
+        tCount[t[i]] = (tCount[t[i]] + 1) || 0;
     }
     for (const key in sCount) {
         if (sCount[key] !== tCount[key]) {
