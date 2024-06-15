8e3cbd2bae7ca9ff81a80c382ff4b43ef49ad78f
irene <panisirene@gmail.com>
2024-06-15 23:41:40 +0000

    [LeetCode Sync] Runtime - 54 ms (75.84%), Memory - 50.6 MB (31.72%)

---
+++ solutions/0020-valid-parentheses/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        '}': '{',
+        ']': '[',
+        ')': '(',
+    };
+
+    for (const char of s) {
+        const isBracket = char in map;
+        if (!isBracket) {
+            stack.push(char);
+            continue;
+        }
+
+        const isEqual = stack[stack.length - 1] === map[char];
+        if (isEqual) {
+            stack.pop();
+            continue;
+        }
+
+        return false;
+    }
+
+    return stack.length === 0;
+};
