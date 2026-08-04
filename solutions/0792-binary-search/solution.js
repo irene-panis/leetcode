866c8906ace93c1646e45bdf2fde971f0c507893
irene <panisirene@gmail.com>
2026-08-04 04:52:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 58.5 MB (9.44%)

--- solutions/0792-binary-search/solution.js
+++ solutions/0792-binary-search/solution.js
@@ -7,13 +7,13 @@ var search = function(nums, target) {
     let l = 0;
     let r = nums.length - 1;
     while (l <= r) {
-        let m = l + (Math.floor((r - l) / 2)); // find distance btwn L and R then divide by 2 for midpoint
-        if (nums[m] > target) { // if nums[m] is greater than target then move right pointer down (since greater values no longer needed)
-            r = m - 1;
-        } else if (nums[m] < target) { // if nums[m] is less than target move left pointer up
-            l = m + 1;
+        const mid = Math.floor((l + r) / 2);
+        if (target > nums[mid]) {
+            l = mid + 1;
+        } else if (target < nums[mid]) {
+            r = mid - 1;
         } else {
-            return m;
+            return mid;
         }
     }
     return -1;
