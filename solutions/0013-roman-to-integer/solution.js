ec1582f8c675be42eb6bb6d3b4df7edb0ee635cd
irene <panisirene@gmail.com>
2026-08-07 03:45:28 +0000

    [LeetCode Sync] Runtime - 4 ms (73.16%), Memory - 62.3 MB (17.06%)

--- solutions/0013-roman-to-integer/solution.js
+++ solutions/0013-roman-to-integer/solution.js
@@ -3,72 +3,24 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
-    var numToAddTo = 0;
-    
-    for (i = 0; i < s.length; i++) {
-        switch(s.charAt(i)) {
-            case 'I':
-                if (i == s.length - 1) {
-                    numToAddTo += 1;
-                    break;
-                }
-                if (s.charAt(i + 1) == 'V') {
-                    numToAddTo += 4;
-                    i++;
-                } else if (s.charAt(i + 1) == 'X') {
-                    numToAddTo += 9;
-                    i++;
-                } else {
-                    numToAddTo += 1;
-                }
-                break;
-            case 'V':
-                numToAddTo += 5;
-                break;
-            case 'X':
-                if (i == s.length - 1) {
-                    numToAddTo += 10;
-                    break;
-                }
-                if (s.charAt(i + 1) == 'L') {
-                    numToAddTo += 40;
-                    i++;
-                } else if (s.charAt(i + 1) == 'C') {
-                    numToAddTo += 90;
-                    i++;
-                } else {
-                    numToAddTo += 10;
-                }
-                break;
-            case 'L':
-                numToAddTo += 50;
-                break;
-            case 'C':
-                if (i == s.length - 1) {
-                    numToAddTo += 100;
-                    break;
-                }
-                if (s.charAt(i + 1) == 'D') {
-                    numToAddTo += 400;
-                    i++;
-                } else if (s.charAt(i + 1) == 'M') {
-                    numToAddTo += 900;
-                    i++;
-                } else {
-                    numToAddTo += 100;
-                }
-                break;
-            case 'D':
-                numToAddTo += 500;
-                break;
-            case 'M':
-                numToAddTo += 1000;
-                break;
+    const map = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000],
+    ]);
+    let numeral = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current = map.get(s[i]);
+        const next = map.get(s[i + 1]);
+        if (current < next) {
+            numeral -= current;
+        } else {
+            numeral += current;
         }
-        
     }
-    
-    return numToAddTo;
-    
+    return numeral;
 };
