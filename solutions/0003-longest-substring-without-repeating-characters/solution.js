2cf4de9ed72e3cb7b4e4ce27cee17e80ace349ff
irene <panisirene@gmail.com>
2026-08-07 03:20:50 +0000

    [LeetCode Sync] Runtime - 101 ms (15.53%), Memory - 62.8 MB (11.71%)

---
+++ solutions/0003-longest-substring-without-repeating-characters/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+        let l = 0;
+        let length = 0;
+        let substring = new Set();
+        for (let r = 0; r < s.length; r++) {
+            if (substring.has(s[r])) {
+                while (substring.has(s[r])) {
+                    substring.delete(s[l]);
+                    l++;
+                }
+                substring.add(s[r]);
+            } else {
+                substring.add(s[r]);
+            }
+            length = Math.max(length, substring.size);
+        }
+        return length;
+};
