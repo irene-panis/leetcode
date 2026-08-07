d8a39670e8f13d85bde6ac483d0f289c283539fe
irene <panisirene@gmail.com>
2026-08-07 07:29:25 +0000

    [LeetCode Sync] Runtime - 3 ms (48.09%), Memory - 56.4 MB (83.37%)

---
+++ solutions/0724-find-pivot-index/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    const prefix = [nums[0]];
+    for (let i = 1; i < nums.length; i++) {
+        prefix.push(nums[i] + prefix[i - 1]);
+    }
+    for (let i = 0; i < prefix.length; i++) {
+        if ((prefix[prefix.length - 1] - prefix[i] == (prefix[i - 1] ?? 0))) {
+            return i;
+        }
+    }
+    return -1;
+};
