88a680c1f373ec2b6fa4fe649bb8e76b3a30064f
irene <panisirene@gmail.com>
2026-09-09 01:33:25 +0000

    [LeetCode Sync] Runtime - 2 ms (70.08%), Memory - 55.5 MB (34.14%)

--- solutions/0001-two-sum/solution.js
+++ solutions/0001-two-sum/solution.js
@@ -7,7 +7,9 @@ var twoSum = function(nums, target) {
     const seen = new Map();
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i];
-        if (seen.has(diff)) return [seen.get(diff), i];
+        if (seen.has(diff)) {
+            return [seen.get(diff), i];
+        }
         seen.set(nums[i], i);
     }
 };
