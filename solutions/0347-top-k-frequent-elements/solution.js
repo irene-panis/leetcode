70d9cf88c44b04a133417a24f1a4591fe51f1a91
irene <panisirene@gmail.com>
2026-09-09 02:10:44 +0000

    [LeetCode Sync] Runtime - 9 ms (81.56%), Memory - 58 MB (88.82%)

--- solutions/0347-top-k-frequent-elements/solution.js
+++ solutions/0347-top-k-frequent-elements/solution.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    const mp = new Map();
+    let counts = new Map();
     for (const num of nums) {
-        mp.set(num, (mp.get(num) ?? 0) + 1);
+        counts.set(num, (counts.get(num) ?? 0) + 1);
     }
-    const arr = Array.from(mp.entries()).sort((a, b) => b[1] - a[1]);
+    const arr = Array.from(counts.entries()).sort((a, b) => b[1] - a[1]);
     return arr.slice(0, k).map(([num]) => num);
 };
