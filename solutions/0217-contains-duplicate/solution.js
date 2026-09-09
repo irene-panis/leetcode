b8095927f361238537d79e9a76a442ee4cad1cc6
irene <panisirene@gmail.com>
2026-09-09 01:23:25 +0000

    [LeetCode Sync] Runtime - 23 ms (66.59%), Memory - 80.1 MB (16.33%)

--- solutions/0217-contains-duplicate/solution.js
+++ solutions/0217-contains-duplicate/solution.js
@@ -5,10 +5,9 @@
 var containsDuplicate = function(nums) {
     const seen = new Set();
     for (const num of nums) {
-        if (seen.has(num)) {
-            return true;
-        }
+        if (seen.has(num)) return true;
         seen.add(num);
     }
     return false;
+
 };
